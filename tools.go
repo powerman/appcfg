c8dfc77004e39c0f28db3b73511e0e9b5ad86d9c
Alex Efros <powerman@powerman.name>
2022-08-21 14:09:02 +0300

    chore(build): update (#66)

--- tools.go
+++ tools.go
@@ -1,11 +1,10 @@
-// +build tools generate
+//go:build generate
 
-//go:generate sh -c "GOBIN=$PWD/.gobincache go install $(sed -n 's/.*_ \"\\(.*\\)\".*/\\1/p' <$GOFILE)"
+//go:generate mkdir -p .buildcache/bin
+//go:generate -command GOINSTALL env "GOBIN=$PWD/.buildcache/bin" go install
 
-package tools
+package appcfg
 
-import (
-	_ "github.com/cheekybits/genny"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/mattn/goveralls"
-)
+//go:generate GOINSTALL github.com/golangci/golangci-lint/cmd/golangci-lint@v1.48.0
+//go:generate GOINSTALL github.com/mattn/goveralls@v0.0.11
+//go:generate GOINSTALL github.com/cheekybits/genny@v1.0.1-0.20200709201058-3e22f1a88ff2
--- values_value_int.go
+++ values_value_int.go
@@ -1,5 +1,5 @@
-//go:generate -command genny sh -c "$(git rev-parse --show-toplevel)/.gobincache/$DOLLAR{DOLLAR}0 \"$DOLLAR{DOLLAR}@\"" genny
-//go:generate genny -in=$GOFILE -out=gen.$GOFILE gen "Port=Int,IntBetween,ListenPort"
+//go:generate -command GENNY sh -c "$(git rev-parse --show-toplevel)/.buildcache/bin/$DOLLAR{DOLLAR}0 \"$DOLLAR{DOLLAR}@\"" genny
+//go:generate GENNY -in=$GOFILE -out=gen.$GOFILE gen "Port=Int,IntBetween,ListenPort"
 //go:generate sed -i -e "\\,^//go:generate,d" gen.$GOFILE
 
 package appcfg
